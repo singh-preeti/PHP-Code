d7a930535b29c5a7aa877f4f880f4039f592551e
singh-preeti <55255569+singh-preeti@users.noreply.github.com>
2021-08-01 14:31:36 +0530

    Add files via upload

---
+++ E-Commerce App/ecart.php
@@ -0,0 +1,19 @@
+<html>
+
+<body>
+link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css
+<-- Google Apis link -->
+<link rel="stylesheet"
+
+href="https://fonts.googleapis.com/css?family=Font+Name">
+
+<?php
+$con =mysqli_Connect();
+?>
+</body>
+</html>
